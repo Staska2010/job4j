a59ff3a4aa51fdc02318fb92972065c1840c7e70
Stanislav Klokov <klokovstas2013@gmail.com>
2019-08-23 14:54:51 +0300

    Ипотека.[#164974]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int monthly, double percent) {
+        int year = 0;
+        amount *= (100 + percent) / 100;
+        while (amount > 0) {
+            for (int i = 0; i < 12; i++) {
+                if (amount < 0) break;
+                amount -= monthly;
+            }
+            year++;
+        }
+        return year;
+    }
+}

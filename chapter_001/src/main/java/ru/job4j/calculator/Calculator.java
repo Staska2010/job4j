4c4424e9d403ccce5ace402c7040af83f8564ccc
Stanislav Klokov <klokovstas2013@gmail.com>
2019-08-23 16:17:25 +0300

    JavaDoc. Code convention[#164970]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,26 +1,43 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculator
+ *
+ * @author Stanislav Klokov
+ */
+
 public class Calculator {
+    /**
+     * Метод, вычисляющий сумму двух чисел
+     *
+     * @param first  - первое число
+     * @param second - второе число
+     */
     public static void add(double first, double second) {
-        double result =  first + second;
-        System.out.println(first +  " + " + second + " = " + result);
+        double result = first + second;
+        System.out.println(first + " + " + second + " = " + result);
     }
 
     public static void div(double first, double second) {
-        double result =  first / second;
-        System.out.println(first +  " / " + second + " = " + result);
+        double result = first / second;
+        System.out.println(first + " / " + second + " = " + result);
     }
 
     public static void multiply(double first, double second) {
-        double result =  first * second;
-        System.out.println(first +  " * " + second + " = " + result);
+        double result = first * second;
+        System.out.println(first + " * " + second + " = " + result);
     }
 
     public static void subtrack(double first, double second) {
-        double result =  first + second;
-        System.out.println(first +  " - " + second + " = " + result);
+        double result = first + second;
+        System.out.println(first + " - " + second + " = " + result);
     }
 
+    /**
+     * Метод main запуска вывода на консоль результатов вычислений.
+     *
+     * @param args - args
+     */
     public static void main(String[] args) {
         add(1, 1);
         multiply(2, 2);

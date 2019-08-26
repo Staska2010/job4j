969dbb1118f76bec5dee07ae91b19038c47ff860
Stanislav Klokov <klokovstas2013@gmail.com>
2019-08-26 17:02:18 +0300

    6.5.1. Сортировка выборкой.[#164989]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -22,4 +22,18 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public int[] sort(int[] data) {
+        for(int i = 0; i < data.length; i++) {
+            int min = data[i];
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[j] < min) {
+                    min = data[j];
+                }
+            }
+            data[indexOf(data, min, i, data.length)] = data[i];
+            data[i] = min;
+        }
+        return data;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,9 +1,11 @@
 package ru.job4j.array;
 
+//import org.junit.Ignore;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+//@Ignore
 public class FindLoopTest {
     @Test
     public void whenArrayHasLengh5Then0() {
@@ -36,6 +38,24 @@ public class FindLoopTest {
         int finish = 4;
         int result = find.indexOf(input, value, start, finish);
         int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSort3() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {3, 2, 1};
+        int[] result = find.sort(input);
+        int[] expect = new int[] {1, 2, 3};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSort5() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {3, 4, 1, 2, 5};
+        int[] result = find.sort(input);
+        int[] expect = new int[] {1, 2, 3, 4, 5};
         assertThat(result, is(expect));
     }
 }

b7d354c1d6ad42dafe8187c61096b476dbf47a7e
Stanislav Klokov <klokovstas2013@gmail.com>
2019-09-03 16:28:22 +0300

    1. Рефакторинг - Расстояние между точками.[#165088]

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -1,17 +1,33 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class PointTest {
     @Test
-    public void distance() {
-        int inX1 = 0;
-        int inX2 = 2;
-        int inY1 = 0;
-        int inY2 = 0;
-        double expected = 2f;
-        double out = Point.distance(inX1, inY1, inX2, inY2);
-        Assert.assertEquals(expected, out, 0.1);
+    public void whenZeroAndTenThenTen() {
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 10);
+        double result = first.distance(second);
+        first.info();
+        second.info();
+        System.out.println(String.format("Result is %s", result));
+        assertThat(result, is(10D));
+    }
+
+    @Test
+    public void whenCheckItself() {
+        Point point = new Point(0, 0);
+        double result = point.distance(point);
+        assertThat(result, is(0D));
+    }
+
+    @Test
+    public void whenShowInfo() {
+        Point first = new Point(1, 1);
+        first.info();
+        Point second = new Point(2, 2);
+        second.info();
     }
 }

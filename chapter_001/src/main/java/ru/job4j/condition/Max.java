69ecc1ca20119b7beb0c209ffe14ff9ab68c28dd
Stanislav Klokov <klokovstas2013@gmail.com>
2019-09-03 21:53:43 +0300

    3. Перегрузить метод max для трех чисел[#165091]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,7 +1,17 @@
 package ru.job4j.condition;
 
 public class Max {
-    public int max(int left, int right) {
-        return left > right ? left : right;
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+
+    public int max(int first, int second, int third) {
+        int tmp = max(second, third);
+        return first > tmp ? first : tmp;
+    }
+
+    public int max(int first, int second, int third, int forth) {
+        int tmp = max(second, third, forth);
+        return first > tmp ? first : tmp;
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -24,4 +24,40 @@ public class MaxTest {
         int result = max.max(2, 2);
         assertThat(result, is(2));
     }
+
+    @Test
+    public void whenMax1To2To3Then3() {
+        Max max = new Max();
+        int result = max.max(1, 2, 3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMax3To2To1Then3() {
+        Max max = new Max();
+        int result = max.max(3, 2, 1);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMax1To2To3To4Then4() {
+        Max max = new Max();
+        int result = max.max(1, 2, 3, 4);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenMax4To3To2To1Then4() {
+        Max max = new Max();
+        int result = max.max(4, 3, 2, 1);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenMax4To4To4To4Then4() {
+        Max max = new Max();
+        int result = max.max(4, 4, 4, 4);
+        assertThat(result, is(4));
+    }
+
 }

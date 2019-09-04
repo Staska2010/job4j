5e7efd83bd901d4a351b179dafb5df6c4fc6317d
Stanislav Klokov <klokovstas2013@gmail.com>
2019-09-04 15:23:55 +0300

    1. Реализация профессий в коде[#165095]

---
+++ chapter_002/src/main/java/ru/job4j/inheritance/Programmer.java
@@ -0,0 +1,19 @@
+package ru.job4j.inheritance;
+
+public class Programmer extends Engineer {
+    private String usedLanguage;
+
+    public void readASpecification(String specificaion) {
+
+    }
+
+    public String writeCode() {
+        String code = "";
+        return code;
+    }
+
+    public boolean makeVerificaton() {
+        boolean isVerificationOk = false;
+        return isVerificationOk;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/inheritance/Surgeon.java
@@ -0,0 +1,22 @@
+package ru.job4j.inheritance;
+
+public class Surgeon extends Doctor {
+    private String qualification;
+    private int numberOfSurgeries;
+
+    public void takePatient() {
+
+    }
+
+    public void makeSurgerie() {
+
+    }
+
+    public void writeReceipt() {
+
+    }
+
+    public void sendToRecovery() {
+
+    }
+}

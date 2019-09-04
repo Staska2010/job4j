34838040ab6f24fa63769d5c0253bbc428f08705
Stanislav Klokov <klokovstas2013@gmail.com>
2019-09-04 16:06:18 +0300

    1. Реализация профессий в коде[#165095] Правки

--- chapter_002/src/main/java/ru/job4j/inheritance/Profession.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/Profession.java
@@ -8,6 +8,18 @@ public class Profession {
     private String education;
     private Date birthDate;
 
+    public Profession(String surname, String name) {
+        this.surname = surname;
+        this.name = name;
+    }
+
+    public Profession(String surname, String name, String education, Date birthDate) {
+        this.surname = surname;
+        this.name = name;
+        this.education = education;
+        this.birthDate = birthDate;
+    }
+
     public String getName() {
         return name;
     }

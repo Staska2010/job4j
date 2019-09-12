612daba9e98e97913309ba1f7e140c85d4f1cdb6
Stanislav Klokov <klokovstas2013@gmail.com>
2019-09-12 11:04:11 +0300

    5. Рефакторинг теста @Before @After[#165071]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertThat;
@@ -9,6 +11,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(stdout);
+    }
+
     @Test
     public void whenDrawSquare() {
         ByteArrayOutputStream out = new ByteArrayOutputStream();

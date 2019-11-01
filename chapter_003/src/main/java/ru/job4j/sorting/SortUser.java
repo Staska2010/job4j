a218391d29490858d56f1793a0808b5c3359f6c9
Stanislav Klokov <klokovstas2013@gmail.com>
2019-11-01 12:30:24 +0300

    2. Сортировка User с использованием Comparator[#165037]

--- chapter_003/src/main/java/ru/job4j/sorting/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sorting/SortUser.java
@@ -1,13 +1,32 @@
 package ru.job4j.sorting;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SortUser {
     public static Set<User> sort(List<User> users) {
         Set<User> result = new TreeSet<>();
         result.addAll(users);
         return result;
+    }
+
+    public static List<User> sortNameLength(List<User> users) {
+        users.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return (o1.getName().length() - o2.getName().length());
+            }
+        });
+        return users;
+    }
+
+    public static List<User> sortByAllFields(List<User> users) {
+        users.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int result = o1.getName().compareTo(o2.getName());
+                return (result == 0) ? o1.getAge() - o2.getAge() : result;
+            }
+        });
+        return users;
     }
 }

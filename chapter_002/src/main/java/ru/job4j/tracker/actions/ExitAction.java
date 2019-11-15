857c1a6eb7f3ba1bf3fc5f1d5a9ae7588be5f447
Stanislav Klokov <klokovstas2013@gmail.com>
2019-11-15 11:00:10 +0300

    Fixes in Tracker

--- chapter_002/src/main/java/ru/job4j/tracker/actions/ExitAction.java
+++ chapter_002/src/main/java/ru/job4j/tracker/actions/ExitAction.java
@@ -1,4 +1,10 @@
-package ru.job4j.tracker;
+package ru.job4j.tracker.actions;
+
+import ru.job4j.tracker.Input;
+import ru.job4j.tracker.Tracker;
+import ru.job4j.tracker.UserAction;
+
+import java.util.function.Consumer;
 
 public class ExitAction implements UserAction {
     @Override
@@ -12,7 +18,7 @@ public class ExitAction implements UserAction {
     }
 
     @Override
-    public boolean execute(Input input, Tracker tracker) {
+    public boolean execute(Input input, Tracker tracker, Consumer<String> output) {
         return false;
     }
 }

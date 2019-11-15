1260fd4fa0bad5647190ab57d95fa22f2f064dfc
Stanislav Klokov <klokovstas2013@gmail.com>
2019-11-15 22:51:25 +0300

    4. Преобразование List в Map.[#164934] - rm distinct

--- chapter_004/src/main/java/ru/job4j/streams/ListToMap.java
+++ chapter_004/src/main/java/ru/job4j/streams/ListToMap.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 public class ListToMap {
     public Map<String, ExtStudent> convert(List<ExtStudent> students) {
         return students.stream()
-                        .distinct()
                         .collect(Collectors.toMap(s -> s.getSurname(), s -> s));
     }
 }

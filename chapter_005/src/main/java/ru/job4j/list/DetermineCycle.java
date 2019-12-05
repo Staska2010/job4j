6c57870843785b6074d891e5041a6835d4621d0b
Stanislav Klokov <klokovstas2013@gmail.com>
2019-12-05 16:35:38 +0300

    5.3.4. Задан связанный список. Определить цикличность.[#200456] Floyd search algorith used

--- chapter_005/src/main/java/ru/job4j/list/DetermineCycle.java
+++ chapter_005/src/main/java/ru/job4j/list/DetermineCycle.java
@@ -1,28 +1,64 @@
 package ru.job4j.list;
 
+/**
+ * Floyd's cycle search algorithm
+ * @param <E>
+ */
 public class DetermineCycle<E> {
-    SimpleArrayList<Node<E>> used = new SimpleArrayList<>();
-
     public boolean hasCycle(Node<E> first) {
-        while (first != null) {
-            if (!contains(first)) {
-                used.add(first);
-            } else {
-                return true;
-            }
-            first = first.next;
-        }
-        return false;
-    }
-
-    private boolean contains(Node<E> node) {
         boolean result = false;
-        for (int i = 0; i < used.getSize(); i++) {
-            if (used.get(i).value == node.value) {
+        Node<E> slow = first;
+        Node<E> fast = first;
+        Node<E> start = null;
+        int length = 0;
+
+        while (true) {
+            slow = slow.next;
+            if (fast.next != null) {
+                fast = fast.next.next;
+            }
+            if (slow == fast) {
                 result = true;
+                start = startOfCycle(slow, fast, first);
+                length = lengthOfCycle(slow, fast);
+                break;
+            }
+            if (slow == null || fast == null) {
+                result = false;
                 break;
             }
         }
         return result;
+    }
+
+    /**
+     * search of start of cycle
+     * @param fast
+     * @param slow
+     * @return  node - start of cycle
+     */
+
+    private Node<E> startOfCycle(Node<E> fast, Node<E> slow, Node<E> first) {
+        slow = first;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
+
+    /**
+     * calculate length of cycle
+     * @param fast
+     * @param slow
+     * @return length of cycle
+     */
+    private int lengthOfCycle(Node<E> fast, Node<E> slow) {
+        int num = 0;
+        while (fast != slow) {
+            fast = fast.next;
+            num++;
+        }
+        return num;
     }
 }

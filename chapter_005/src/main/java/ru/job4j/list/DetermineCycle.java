286c6ffaaabba7bc24a580e6425166ea7bd69813
Stanislav Klokov <klokovstas2013@gmail.com>
2019-12-09 11:44:06 +0300

    5.3.4. Задан связанный список. Определить цикличность.[#200456] Simplified method

--- chapter_005/src/main/java/ru/job4j/list/DetermineCycle.java
+++ chapter_005/src/main/java/ru/job4j/list/DetermineCycle.java
@@ -2,6 +2,7 @@ package ru.job4j.list;
 
 /**
  * Floyd's cycle search algorithm
+ *
  * @param <E>
  */
 public class DetermineCycle<E> {
@@ -9,56 +10,15 @@ public class DetermineCycle<E> {
         boolean result = false;
         Node<E> slow = first;
         Node<E> fast = first;
-        Node<E> start = null;
-        int length = 0;
 
-        while (true) {
+        while (fast != null && fast.next != null) {
             slow = slow.next;
-            if (fast.next != null) {
-                fast = fast.next.next;
-            }
+            fast = fast.next.next;
             if (slow == fast) {
                 result = true;
-                start = startOfCycle(slow, fast, first);
-                length = lengthOfCycle(slow, fast);
-                break;
-            }
-            if (slow == null || fast == null) {
-                result = false;
                 break;
             }
         }
         return result;
-    }
-
-    /**
-     * search of start of cycle
-     * @param fast
-     * @param slow
-     * @return  node - start of cycle
-     */
-
-    private Node<E> startOfCycle(Node<E> fast, Node<E> slow, Node<E> first) {
-        slow = first;
-        while (fast != slow) {
-            fast = fast.next;
-            slow = slow.next;
-        }
-        return fast;
-    }
-
-    /**
-     * calculate length of cycle
-     * @param fast
-     * @param slow
-     * @return length of cycle
-     */
-    private int lengthOfCycle(Node<E> fast, Node<E> slow) {
-        int num = 0;
-        while (fast != slow) {
-            fast = fast.next;
-            num++;
-        }
-        return num;
     }
 }
